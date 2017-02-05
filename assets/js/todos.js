0343c26caf70aef2b942b14c0d2d932d24337252
krisnotchris <krishenderson@gmail.com>
2017-02-04 18:33:48 -0600

    added fadeout functionality on li

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $('li').on('click', function(){
   $(this).toggleClass("completed");
 })
+
+// Click on X to delete Todo
+$("span").on("click", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+})
